6a24de68bda4f7836517fc543f8dc9d248e39b76
UAhmadSoft <umadahmad1928@gmail.com>
2023-01-06 17:43:43 +0500

    Refactored Routes

---
+++ controllers/userController.js
@@ -0,0 +1,116 @@
+const User = require('../models/userModel');
+
+exports.getAllUsers = async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    users,
+  });
+};
+
+exports.deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  console.log('id', id);
+
+  // const user = await User.findOneAndDelete({
+  //   email : 'umad1@gmail.com'
+  // });
+  // const user = await User.findOne({
+  //   _id: id,
+  // });
+  const user = await User.findByIdAndDelete(id);
+
+  // if(user === null)
+  if (!user) {
+    return res.status(404).json({
+      staus: 'failed',
+      message: `No user found against id ${id}`,
+    });
+  }
+
+  res.status(200).json({
+    staus: 'success',
+    // user  : user ,
+    user,
+  });
+
+  // console.log(req.params);
+};
+
+exports.getUser = async (req, res) => {
+  console.log('getUsr');
+  const { id } = req.params;
+
+  console.log('id', id);
+
+  // const user = await User.findOne({
+  //   email : 'umad1@gmail.com'
+  // });
+  // const user = await User.findOne({
+  //   _id: id,
+  // });
+  const user = await User.findById(id);
+
+  // if(user === null)
+  if (!user) {
+    return res.status(404).json({
+      staus: 'failed',
+      message: `No user found against id ${id}`,
+    });
+  }
+
+  res.status(200).json({
+    staus: 'success',
+    // user  : user ,
+    user,
+  });
+
+  // console.log(req.params);
+};
+
+exports.createUser = async (req, res) => {
+  // new user
+  console.log('req.body', req.body);
+
+  const user = await User.create({
+    ...req.body,
+  });
+  // const user = await User.create({
+  //   fistName : req.body.fistName
+  // })
+
+  res.status(201).json({
+    status: 'success',
+    user,
+  });
+};
+
+exports.updateUser = async (req, res) => {
+  const { id } = req.params;
+
+  // const user = await User.findOneAndUpdate({
+  //   _id : id
+  // }, {
+  //   ...req.body
+  // }, {
+  //   new : true,
+  // runValidators : false
+  // })
+  const user = await User.findByIdAndUpdate(
+    id,
+    {
+      ...req.body,
+    },
+    {
+      new: true,
+      // runValidators : false
+    }
+  );
+
+  res.status(202).json({
+    status: 'success',
+    user,
+  });
+};
