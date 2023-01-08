95858819c209115be3e2eedc73a43f09eaa60161
UAhmadSoft <umadahmad1928@gmail.com>
2023-01-08 14:02:06 +0500

    Changed PORT For deploymeny

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.all('*', (req, res) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Listening to server ${PORT} `);
 });
